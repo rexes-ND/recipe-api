66a1d1e58bac706fa6e2262dcf60fd6e9f26cec9
Erkhes <erkhes.nyamsaikhan@gmail.com>
2023-05-07 18:23:06 +0900

    Add API endpoints related to users

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,11 @@
 """
 Serializers for the user API View.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
 
 # serializer: convert obj to/from obj
 from rest_framework import serializers
@@ -19,3 +23,43 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        # Password is going to be optional
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'pass'},
+        trim_whitespace=False,
+    )
+
+    # This method will be called at validation step by the view
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _('Unable to authenticate with \
+                provided provided credentials')
+            # The view will automatically convert it to HTTP 400 response
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -1,11 +1,42 @@
 """
 Views for the user API.
 """
-from rest_framework import generics
+from rest_framework import (
+    generics,
+    authentication,
+    permissions
+)
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+    # Customizer auth using email and password
+    serializer_class = AuthTokenSerializer
+    # For nice UI
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+# For retrieving and updating the obj in DB
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    # Make sure user using the API is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    # HTTP GET
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
